1e56dab91834ed9068c34e830d5f90b44c1545fc
Jacek Kunicki <jacek@kunicki.org>
2014-02-07 14:28:48 +0100

    BeaconImageProvider skeleton

---
+++ Demos/src/main/java/com/estimote/examples/demos/BeaconImageProvider.java
@@ -0,0 +1,13 @@
+package com.estimote.examples.demos;
+
+import android.graphics.Bitmap;
+
+import com.estimote.sdk.Beacon;
+
+public class BeaconImageProvider {
+
+    public Bitmap getBitmapForBeacon(Beacon beacon) {
+        return null;
+    }
+
+}
