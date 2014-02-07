4c7df11abe4b0d06b968066b164f51339481eb40
Jacek Kunicki <jacek@kunicki.org>
2014-02-07 14:55:51 +0100

    BeaconImageProvider returns image URL

---
+++ Demos/src/main/java/com/estimote/examples/demos/BeaconImage.java
@@ -0,0 +1,24 @@
+package com.estimote.examples.demos;
+
+public class BeaconImage {
+
+    String name;
+
+    String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
--- Demos/src/main/java/com/estimote/examples/demos/BeaconImageProvider.java
+++ Demos/src/main/java/com/estimote/examples/demos/BeaconImageProvider.java
@@ -1,13 +1,21 @@
 package com.estimote.examples.demos;
 
-import android.graphics.Bitmap;
-
 import com.estimote.sdk.Beacon;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 public class BeaconImageProvider {
 
-    public static Bitmap getBitmapForBeacon(Beacon beacon) {
-        return null;
+    private static RestTemplate restTemplate = new RestTemplate();
+
+    static {
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+    }
+
+    public static String getBitmapForBeacon(Beacon beacon) {
+        BeaconImage beaconImage = restTemplate.getForObject("", BeaconImage.class);
+        return beaconImage.getUrl();
     }
 
 }
